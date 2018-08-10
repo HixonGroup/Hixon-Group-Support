11fee23937c682522567cb24f51ddba5b2e256b9
Chris Hampson <chris.hampson@hixongroup.com>
2018-08-10 12:44:49 +0100

    Add project files.

---
+++ Hixon/Hixon/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hixon
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+            Button hixongroupweb = new Button
+            {
+                Text = "Hixon Group Website",
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Start
+            };
+
+            hixongroupweb.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new HixonGroupWeb());
+            };
+
+            Content = hixongroupweb;
+        }
+	}
+}
