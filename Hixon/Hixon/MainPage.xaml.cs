78442ab83d878b6eb9f115e3f62ba05f7aafb3d3
Chris Hampson <chris.hampson@hixongroup.com>
2018-08-16 10:11:09 +0100

    Working App???

--- Hixon/Hixon/MainPage.xaml.cs
+++ Hixon/Hixon/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hixon.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,24 +12,19 @@ namespace Hixon
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage : ContentPage
-	{
+    {
 		public MainPage ()
 		{
 			InitializeComponent ();
-
-            Button hixongroupweb = new Button
-            {
-                Text = "Hixon Group Website",
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                VerticalOptions = LayoutOptions.Start
-            };
-
-            hixongroupweb.Clicked += async (sender, args) =>
-            {
-                await Navigation.PushAsync(new HixonGroupWeb());
-            };
-
-            Content = hixongroupweb;
         }
-	}
+
+        async void Hixongroupweb(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HixonGroupWeb());
+        }
+        async void Zendeskweb(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ZendeskSupportWeb());
+        }
+    }
 }
