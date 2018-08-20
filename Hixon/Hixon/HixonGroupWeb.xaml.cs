5d94ce8e6072e1f521389fdcdc69eabec1daf0c5
Chris Hampson <chris.hampson@hixongroup.com>
2018-08-20 12:34:17 +0100

    V:0.3

---
+++ Hixon/Hixon/HixonGroupWeb.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Hixon
+{
+	public partial class HixonGroupWeb : ContentPage
+	{
+		public HixonGroupWeb ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Backbtn(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+	}
+}
---
+++ Hixon/Hixon/ZendeskSupportWeb.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Hixon
+{
+	public partial class ZendeskSupportWeb : ContentPage
+	{
+		public ZendeskSupportWeb ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Backbtn(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
